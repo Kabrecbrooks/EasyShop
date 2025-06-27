c1320d1de77414e7528a7d90a9effc22fe8ee604
KabreaCranerBrooks <k.cranerbrooks@my.yearupunited.org>
2025-06-26 22:12:39 -0700

    some improvements

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -32,8 +32,7 @@ public class CategoriesController{
 
     // add the appropriate annotation for a get action
     @GetMapping()
-    public List<Category> getAll()
-    {
+    public List<Category> getAll()    {
         // find and return all categories
         return categoryDao.getAllCategories();
     }
